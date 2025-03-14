bf2096217a3c301d54dc0f3d4d458e7754670bc1
Wailiam-li <1374438718@qq.com>
2025-03-14 17:17:38 +0800

    2025.03.14 第三次

--- mall-admin/src/main/java/com/macro/mall/config/SwaggerConfig.java
+++ mall-admin/src/main/java/com/macro/mall/config/SwaggerConfig.java
@@ -21,8 +21,8 @@ public class SwaggerConfig extends BaseSwaggerConfig {
         return SwaggerProperties.builder()
                 .apiBasePackage("com.macro.mall.controller")
                 .title("mall后台系统")
-                .description("mall后台相关接口文档. 管理员用户登录：账号：admin,密码：macro123")
-                .description("注：根据以上账号密码到“后台用户管理”的/admin/login接口获取token，到Authorize中授权，注：头部需要带上“Bearer”")
+                .description("mall后台相关接口文档. 管理员用户登录：账号：admin,密码：macro123。" +
+                        "注：根据以上账号密码到“后台用户管理”的/admin/login接口获取token，到Authorize中授权，注：头部需要带上“Bearer”")
                 .contactName("macro")
                 .version("1.0")
                 .enableSecurity(true)
--- mall-portal/src/main/java/com/macro/mall/portal/MallPortalApplication.java
+++ mall-portal/src/main/java/com/macro/mall/portal/MallPortalApplication.java
@@ -3,6 +3,9 @@ package com.macro.mall.portal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 前台商城系统接口
+ */
 @SpringBootApplication(scanBasePackages = "com.macro.mall")
 public class MallPortalApplication {
 
