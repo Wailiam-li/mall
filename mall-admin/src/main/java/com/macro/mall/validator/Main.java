d37edf09fddfaccfc2adb16211a8c01c67420a3b
Liweiwei <1374438718@qq.com>
2025-05-01 16:52:51 +0800

    9.18

---
+++ mall-admin/src/main/java/com/macro/mall/validator/Main.java
@@ -0,0 +1,14 @@
+package com.macro.mall.validator;
+
+/**
+ * @author Liweiwei
+ * @Date 2024/9/21 16:13
+ * @Description :
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("liwei is handsome!");
+
+    }
+
+}
